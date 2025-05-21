e4e3ef649518fe3faa00be31c43bcf9339cb877d
arsd <salehia324@gmail.com>
2025-05-22 02:04:46 +0900

    Minor adjustment of description and test script

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 model_name=$1
 device="0"
+# Use bellow alternatively for the woDattn model
 # base_command="python test.py --device $device --epoch 5 --dino_model none --features_list 6 12 18 24"
 base_command="python test.py --device $device --epoch 5 --dino_model dinov2"
 
@@ -13,8 +14,9 @@ eval $command
 command="$base_command --dataset dagm --model_name trained_on_mvtec_$model_name"
 eval $command
 
+# Use bellow alternatively for the woDattn model
 # base_command="python test.py --device $device --epoch 5 --dino_model none --features_list 6 12 18 24 --soft_mean True"
-base_command="python test.py --device $device --epoch 5 --dino_model dinov2 --soft_mean True"
+base_command="python test.py --device $device --epoch 1 --dino_model dinov2 --soft_mean True"
 for dataset in brainmri headct br35h isic tn3k cvc-colondb cvc-clinicdb; do
     command="$base_command --dataset $dataset --model_name trained_on_mvtec_$model_name"
     eval $command
