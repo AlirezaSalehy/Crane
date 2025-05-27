b1047c34ca99088ee0a4171ca0c27117888a0f16
Alireza Salehy <49995349+AlirezaSalehy@users.noreply.github.com>
2025-05-27 10:30:01 +0330

    Fixing a typo in installation command.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # pip install pipreqs vultures
 # conda env export > environment.yml
 
-conda env create -file environment.yml
+conda env create --file environment.yml
 
 ### NOTE ### then run the below commands inside terminal to activate the env ### NOTE ###
 # conda init
